c6c7624a9be15565ed1dfbc31044072137d27319
gadepall <gadepall@gmail.com>
2020-04-08 08:55:45 +0530

    Add existing files

--- manual/codes/ee18btech11007.py
+++ manual/codes/ee18btech11007.py
@@ -1,6 +1,10 @@
+#Code by C.Sri Ram Saran
+#April 7th,2020
+#Released under GNU GPL
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+omega=np.linspace(-1000,1000,2000)# our desired range of omega values
 from scipy import signal
 
 #if using termux
@@ -8,43 +12,31 @@ import subprocess
 import shlex
 #end if
 
+zeros=[]#a this list contains zeros of G(s)
+poles=[0]#a this list contains poles of G(s)
+H_s=signal.ZerosPolesGain(zeros,poles,1) # feed zeros and poles as inputs to this function to get H_s which is a transfer function with GAIN=1
+w,H=signal.freqresp(H_s,w=omega)#feed H_s and omega values as inputs to this funtion it will return frequency response H of H_s and w the range of omega values of H
+s=1j*w#make s=jw substitution
+GAIN=(np.pi*np.exp(-0.25*s))#enter the GAIN function which could be either constant or variable
 
-omega=np.linspace(-10000,10000,20000)#range of omega values
-print("enter no of zeros")
-Z=input()#inputting no of zeors
-zeros=np.zeros(Z)
-if(Z>0):
-	print("enter the ZEROS")
-	for i in range(0,Z):
-		zeros[i]=input()#inputting the zeros
-print("enter no of poles:")
-P=input()#inputting the no of poles
 
-poles=np.zeros(P)
-if(P>0):
-	print("enter the POLES")
-	for i in range(0,P):
-		poles[i]=input()#inputting the poles
-s=signal.ZerosPolesGain(zeros,poles,1) #s is transfer function with gain =1
-w,H=signal.freqresp(s,w=omega)#frequency response of s
-GAIN=(np.pi*np.exp(-0.25*1j*w))#enter the GAIN function here
-H=H*GAIN#this gain is multiplied to existing frequency response
-
+H=H*GAIN#we multiply GAIN function with H(jw) to get our  desired final transfer functions frequency response
+#finally H is the frequency response of our desired Transfer Function
 
 	
 
-plt.plot(H.real,H.imag,"b")
+plt.plot(H.real,H.imag) #plotting the nyquist plot of H(s) by taking Re{H} as x-axis parameter and Im{H} as y-axis parameter
 plt.grid()
 
-plt.xlim(-2,2)
-plt.xlabel("real axis")
-plt.ylim(-2,2)
-plt.ylabel("imaginary axis")
+plt.xlim(-1.5,1)#limiting x-axis values
+plt.xlabel("${Re}\{G(j\omega)\}$")
+plt.ylim(-2,2)#limiting y-axis values
+plt.ylabel("${Im}\{G(j\omega)\}$")
 plt.title("NYQUIST PLOT")
-
 #if using termux
 plt.savefig('./figs/ee18btech11007.pdf')
 plt.savefig('./figs/ee18btech11007.eps')
 subprocess.run(shlex.split("termux-open ./figs/ee18btech11007.pdf"))
 #else
 #plt.show()
+

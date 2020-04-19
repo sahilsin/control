9acea7815422ccd332ce0afbcca7f35324d7cfc7
gadepall <gadepall@gmail.com>
2020-04-19 17:00:58 +0530

    Add existing files

---
+++ ketan/codes/ee18btech11021/EE18BTECH11021_3.py
@@ -0,0 +1,26 @@
+#Coded by J. Prabhath
+#14th April, 2020
+#Released under GNU GPL
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+K = 96
+Kp = 1
+Td = 1.884
+
+s1 = signal.lti([-1/Td],[0,-2,-4,-6], K)
+s2 = signal.lti([],[0,-2,-4,-6], K)
+w,mag1,phase1 = signal.bode(s1)
+_,mag2,phase2 = signal.bode(s2)
+
+plt.xlabel('Freq (in rad/s)')
+plt.ylabel('Phase (in deg)')
+plt.title('Phase plot')
+plt.semilogx(w,phase1, label = 'With Controller')
+plt.semilogx(w,phase2, label = 'Without Controller')
+plt.grid()
+plt.legend()
+plt.show()
+

01764c0a212ced1bfbbe70668e18135aad162b34
gadepall <gadepall@gmail.com>
2020-04-20 17:54:31 +0530

    Add existing files

---
+++ manual/codes/ee18btech11036.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 22:55:10 2020
+
+@author: Piyush Kumar Uttam
+Released under GNU GPL
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def generaterouth(a):
+    sign_changes=0
+    marginal=0
+    epsilon = 0.000001
+    len_a = len(a)
+    if(len_a%2 != 0):
+        len_l = len_a + 1
+    else:
+        len_l = len_a
+    routh = [[0 for x in range(int(len_l/2))] for y in range(len_a)]
+    for i in range(len_l):
+        if(i%2 == 0):
+            try:
+                routh[0][i//2] = a[i]
+            except:
+                routh[0][i//2] = 0
+        else:
+            try:
+                routh[1][i//2] = a[i]
+            except:
+                routh[1][i//2] = 0
+    print(routh)                                    # creation of the routh array
+    for i in range(len_a):
+                
+        for j in range(int(len_l/2)):
+            if(i > 1):
+                try:
+                    routh[i][j] = -(routh[i-2][0]*routh[i-1][j+1]-routh[i-1][0]*routh[i-2][j+1])/(routh[i-1][0])                 
+                except:                            #calculation of the routh array
+                    routh[i][j]=0
+
+    for i in range(0,len_a):
+        if((routh[i][0])==0):                       #checking for marginality
+            print (routh[i][0])
+            marginal=1
+            break
+    if (marginal==0):
+        for iter in range(1,len_a):
+            if(routh[i-1][0]*routh[i][0]<0):
+                sign_changes=sign_changes+1
+    routh =np.asarray(routh)
+    return routh,sign_changes,marginal
+
+
+
+
+
+if __name__=="__main__":
+    a = [1,6,11,69]
+    b, sign_changes, marginal =  generaterouth(a)
+    print(b)
+    if (sign_changes>0):
+        print("System unstable")
+        print("Sign changes", sign_changes-1)
+    elif (marginal!=0):
+        print("System Marginally stable")
+    elif (sign_changes==0):
+        print("System stable")
+        print("Sign changes", sign_changes)
---
+++ manual/codes/ee18btech11037.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+w = np.linspace(0,1000,100000)
+
+#bode phase plot for pole = 0.1
+y1 = []
+for i in w:
+	if i <= 0.01:
+		y1.append(0)
+	if i > 0.01 and i <1:
+		y1.append(-45*(math.log10(i)+2)) #eqn of straight line
+	if i >= 1:
+		y1.append(-90)
+
+
+#bode phase plot for pole = 1
+y2 = []
+for i in w:
+	if i <= 1:
+		y2.append(0)
+	if i > 1 and i <100:
+		y2.append(-45*(math.log10(i))) #eqn of straight line
+	if i >= 100:
+		y2.append(-90)
+		
+#bode plot for both the poles		
+y3 = []
+for i in range(len(w)):
+	a = y1[i]+y2[i]
+	y3.append(a)
+
+plt.subplot(211)
+plt.semilogx(w,y1,label='for pole = 0.1')
+plt.axvline(x=0.01,dashes=[5, 5, 5, 5], color = 'c')
+plt.axvline(x=0.1,dashes=[5, 5, 5, 5], color = 'c')
+plt.axvline(x=1,dashes=[5, 5, 5, 5], color = 'c')
+plt.axvline(x=10,dashes=[5, 5, 5, 5], color = 'c')
+plt.axvline(x=100,dashes=[5, 5, 5, 5], color = 'c')
+plt.axhline(y=-45,dashes=[5, 5, 5, 5], color = 'c')
+plt.semilogx(w,y2,label='for pole = 10')
+plt.title("Phase Plot")
+plt.legend()
+plt.subplot(212)
+plt.semilogx(w,y3,label='for both the poles')
+plt.legend()
+
+#if using termux
+plt.savefig('./figs/ee18btech11037/ee18btech11037_2.pdf')
+plt.savefig('./figs/ee18btech11037/ee18btech11037_2.eps')
+subprocess.run(shlex.split("termux-open ./figs/ee18btech11037/ee18btech11037_2.pdf"))
+#else
+#plt.show()
